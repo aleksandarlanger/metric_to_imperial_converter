6cf8203f2fcecc0cf915599926f9c03fb4548d6b
Aleksandar Langer <alexandar.langer@yahoo.com>
2022-02-25 14:59:31 +0100

    NaN logic fix
    - Switching to type  "number" solved the NaN issue.
    
    - Some CSS scaling changes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const convertBtn = document.querySelector("#convertBtn");
 const switchUnits = document.querySelector("#unit__switch");
 const nanAlert = document.querySelector(".nan_alert");
+const display = document.querySelector("#userInput");
 let numberToConvert = 0;
 let numberToBeConverted = [];
 let switcher = false;
@@ -15,13 +16,13 @@ function converter() {
     numberToBeConverted.push(numberToConvert);
     for (const userNumber of numberToBeConverted) {
       document.querySelector(".metric--value--lenght").textContent = (
-        userNumber / 3.28084
+        userNumber * 3.28084
       ).toFixed(2);
       document.querySelector(".metric--value--volume").textContent = (
-        userNumber / 0.264172
+        userNumber * 0.264172
       ).toFixed(2);
       document.querySelector(".metric--value--mass").textContent = (
-        userNumber / 2.20462
+        userNumber * 2.20462
       ).toFixed(2);
       document.querySelector(
         ".imperial--value--lenght"
@@ -34,36 +35,33 @@ function converter() {
       ).textContent = `${numberToConvert}`;
       numberToBeConverted = [];
       switcher = true;
-      document.querySelector(".unit__text").textContent = "Metric to";
-      document.querySelector(".unit__text--to").textContent = "Imperial";
-      document.querySelector(".unit__reversem").textContent = "Feet";
-      document.querySelector(".unit__reversel").textContent = "Gallons";
-      document.querySelector(".unit__reversek").textContent = "Pounds";
-      document.querySelector(".unit__reversef").textContent = "Meters";
-      document.querySelector(".unit__reverseg").textContent = "Litters";
-      document.querySelector(".unit__reversep").textContent = "Kilograms";
-    }
-
-    if (isNaN(numberToConvert)) {
-      alert("PLEASE ENTER A VALID NUMBER");
-      numberToConvert = 0;
     }
   }
+
+  document.querySelector(".unit__text").textContent = "Metric to";
+  document.querySelector(".unit__text--to").textContent = "Imperial";
+  document.querySelector(".unit__reversem").textContent = "Feet";
+  document.querySelector(".unit__reversel").textContent = "Gallons";
+  document.querySelector(".unit__reversek").textContent = "Pounds";
+  document.querySelector(".unit__reversef").textContent = "Meters";
+  document.querySelector(".unit__reverseg").textContent = "Litters";
+  document.querySelector(".unit__reversep").textContent = "Kilograms";
 }
 
 function reverseCalc() {
   numberToConvert = parseFloat(document.querySelector("#userInput").value);
   numberToBeConverted.push(numberToConvert);
+
   if ((switcher === true) | (startState === true)) {
     for (const userNumber of numberToBeConverted) {
       document.querySelector(".metric--value--lenght").textContent = (
-        userNumber * 3.28084
+        userNumber / 3.28084
       ).toFixed(2);
       document.querySelector(".metric--value--volume").textContent = (
-        userNumber * 0.264172
+        userNumber / 0.264172
       ).toFixed(2);
       document.querySelector(".imperial--value--mass").textContent = (
-        userNumber * 2.20462
+        userNumber / 2.20462
       ).toFixed(2);
       document.querySelector(
         ".imperial--value--lenght"
@@ -74,6 +72,7 @@ function reverseCalc() {
       document.querySelector(
         ".imperial--value--mass"
       ).textContent = ` ${numberToConvert}`;
+
       document.querySelector(".unit__text").textContent = "Imperial to";
       document.querySelector(".unit__text--to").textContent = "Metric";
       document.querySelector(".unit__reversem").textContent = "Meters";
@@ -84,11 +83,6 @@ function reverseCalc() {
       document.querySelector(".unit__reversep").textContent = "Pound";
       numberToBeConverted = [];
       switcher = false;
-    }
-
-    if (isNaN(numberToConvert)) {
-      alert("PLEASE ENTER A VALID NUMBER");
-      numberToConvert = 0;
     }
   } else {
     converter();
