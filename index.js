e9b5b906f1d5484537d36e145a698ec168cb2718
Aleksandar Langer <alexandar.langer@yahoo.com>
2022-02-25 10:49:44 +0100

    Code refactoring
    - Code refactoring
    - Change in logic
    - Design improvements

--- index.js
+++ index.js
@@ -1,112 +1,90 @@
-let numberToConvert = 0;
-const dispay = document.querySelector("#userInput");
 const convertBtn = document.querySelector("#convertBtn");
-
-// .................... unit holders ...........................
-
-const imperialLenght = document.querySelector(".metric--value--lenght");
-const imperialVolume = document.querySelector(".metric--value--volume");
-const imperialMass = document.querySelector(".metric--value--mass");
-
-const valueOfConvertm = document.querySelector(".imperial--value--lenght");
-const valueOfConvertl = document.querySelector(".imperial--value--volume");
-const valueOfConvertk = document.querySelector(".imperial--value--mass");
-
-// ................... text placeholder variables........................
-
-let unitText = document.querySelector(".unit__text");
-let unitTextTo = document.querySelector(".unit__text--to");
-let unitReverseM = document.querySelector(".unit__reversem");
-let unitReverseL = document.querySelector(".unit__reversel");
-let unitReverseK = document.querySelector(".unit__reversek");
-let unitReverseF = document.querySelector(".unit__reversef");
-let unitReverseG = document.querySelector(".unit__reverseg");
-let unitReverseP = document.querySelector(".unit__reversep");
-
-// .................... other variables ............................
-
 const switchUnits = document.querySelector("#unit__switch");
-let metricOn = false;
-let imperialOn = true;
-let unitSwitcher = false;
-let convertDoneMetric = false;
-let convertDoneImperial = false;
-
 const nanAlert = document.querySelector(".nan_alert");
+let numberToConvert = 0;
+let numberToBeConverted = [];
+let switcher = false;
+let startState = true;
 
-// ............ Logic ..........................................................
+convertBtn.addEventListener("click", converter);
+switchUnits.addEventListener("click", reverseCalc);
 
-convertBtn.addEventListener("click", function () {
-  if (unitSwitcher === false) {
-    convertDoneMetric = true;
-    convertValue();
-  } else {
-    convertValueReverse();
-  }
-});
+function converter() {
+  if (switcher === false) {
+    numberToConvert = parseFloat(document.querySelector("#userInput").value);
+    numberToBeConverted.push(numberToConvert);
+    for (const userNumber of numberToBeConverted) {
+      document.querySelector(".metric--value--lenght").textContent = (
+        userNumber / 3.28084
+      ).toFixed(2);
+      document.querySelector(".metric--value--volume").textContent = (
+        userNumber / 0.264172
+      ).toFixed(2);
+      document.querySelector(".metric--value--mass").textContent = (
+        userNumber / 2.20462
+      ).toFixed(2);
+      document.querySelector(
+        ".imperial--value--lenght"
+      ).textContent = ` ${numberToConvert}`;
+      document.querySelector(
+        ".imperial--value--volume"
+      ).textContent = ` ${numberToConvert}`;
+      document.querySelector(
+        ".imperial--value--mass"
+      ).textContent = `${numberToConvert}`;
+      numberToBeConverted = [];
+      switcher = true;
 
-switchUnits.addEventListener("click", function () {
-  unitSwitcher = true;
-  imperialOn = false;
-  document.querySelector("#userInput").placeholder = "imperial unit";
-
-  if (unitSwitcher === true && imperialOn === false) {
-    unitText.textContent = "Imperial";
-    unitTextTo.textContent = "Metric";
-
-    unitReverseM.textContent = "Meters";
-    unitReverseL.textContent = "Liters";
-    unitReverseK.textContent = "Kilograms";
-
-    unitReverseF.textContent = "Feet";
-    unitReverseG.textContent = "Gallon";
-    unitReverseP.textContent = "Pound";
-
-    document.querySelector("#user");
-
-    convertValueReverse();
-  } else if (convertDoneMetric === true) {
-    convertValue();
-  }
-});
-
-function convertValue() {
-  numberToConvert = parseFloat(document.querySelector("#userInput").value);
-
-  if (isNaN(numberToConvert)) {
-    alert("Please enter a valid number");
-  } else if (unitSwitcher != true) {
-    let feetConverted = numberToConvert * 3.28084;
-    imperialLenght.textContent = feetConverted.toFixed(2);
-
-    let gallonConvert = numberToConvert * 0.264172;
-    imperialVolume.textContent = gallonConvert.toFixed(2);
-
-    let poundConvert = numberToConvert * 2.20462;
-    imperialMass.textContent = poundConvert.toFixed(2);
-
-    valueOfConvertm.textContent = ` ${numberToConvert}`;
-    valueOfConvertl.textContent = ` ${numberToConvert}`;
-    valueOfConvertk.textContent = ` ${numberToConvert}`;
+      document.querySelector(".unit__text").textContent = "Metric to";
+      document.querySelector(".unit__text--to").textContent = "Imperial";
+      document.querySelector(".unit__reversem").textContent = "Feet";
+      document.querySelector(".unit__reversel").textContent = "Gallons";
+      document.querySelector(".unit__reversek").textContent = "Pounds";
+      document.querySelector(".unit__reversef").textContent = "Meters";
+      document.querySelector(".unit__reverseg").textContent = "Litters";
+      document.querySelector(".unit__reversep").textContent = "Kilograms";
+    }
   }
 }
 
-function convertValueReverse() {
+function reverseCalc() {
   numberToConvert = parseFloat(document.querySelector("#userInput").value);
-  if (isNaN(numberToConvert)) {
-    alert("Please enter a valid number");
+  numberToBeConverted.push(numberToConvert);
+
+  if ((switcher === true) | (startState === true)) {
+    for (const userNumber of numberToBeConverted) {
+      document.querySelector(".metric--value--lenght").textContent = (
+        userNumber * 3.28084
+      ).toFixed(2);
+      document.querySelector(".metric--value--volume").textContent = (
+        userNumber * 0.264172
+      ).toFixed(2);
+      document.querySelector(".imperial--value--mass").textContent = (
+        userNumber * 2.20462
+      ).toFixed(2);
+      document.querySelector(
+        ".imperial--value--lenght"
+      ).textContent = ` ${numberToConvert}`;
+      document.querySelector(
+        ".imperial--value--volume"
+      ).textContent = ` ${numberToConvert}`;
+      document.querySelector(
+        ".imperial--value--mass"
+      ).textContent = ` ${numberToConvert}`;
+
+      document.querySelector(".unit__text").textContent = "Imperial to";
+      document.querySelector(".unit__text--to").textContent = "Metric";
+      document.querySelector(".unit__reversem").textContent = "Meters";
+      document.querySelector(".unit__reversel").textContent = "Liters";
+      document.querySelector(".unit__reversek").textContent = "Kilograms";
+      document.querySelector(".unit__reversef").textContent = "Feet";
+      document.querySelector(".unit__reverseg").textContent = "Gallon";
+      document.querySelector(".unit__reversep").textContent = "Pound";
+
+      numberToBeConverted = [];
+      switcher = false;
+    }
   } else {
-    let feetConverted = numberToConvert / 3.28084;
-    imperialLenght.textContent = feetConverted.toFixed(2);
-
-    let gallonConvert = numberToConvert / 0.264172;
-    imperialVolume.textContent = gallonConvert.toFixed(2);
-
-    let poundConvert = numberToConvert / 2.20462;
-    imperialMass.textContent = poundConvert.toFixed(2);
-
-    valueOfConvertm.textContent = ` ${numberToConvert}`;
-    valueOfConvertl.textContent = ` ${numberToConvert}`;
-    valueOfConvertk.textContent = ` ${numberToConvert}`;
+    converter();
   }
 }
